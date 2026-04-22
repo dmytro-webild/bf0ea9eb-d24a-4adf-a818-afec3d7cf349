8a1981af7675329aa1dc26735baf4632025c9b6e
bender <kudin.dima@gmail.com>
2026-04-22 09:56:55 +0000

    Switch to version 2: modified src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import ReactLenis from "lenis/react";
 import ContactCenter from '@/components/sections/contact/ContactCenter';
 import FeatureBento from '@/components/sections/feature/FeatureBento';
 import FooterLogoEmphasis from '@/components/sections/footer/FooterLogoEmphasis';
-import HeroBillboard from '@/components/sections/hero/HeroBillboard';
+import HeroSplitDoubleCarousel from '@/components/sections/hero/HeroSplitDoubleCarousel';
 import MetricCardEleven from '@/components/sections/metrics/MetricCardEleven';
 import NavbarLayoutFloatingOverlay from '@/components/navbar/NavbarLayoutFloatingOverlay/NavbarLayoutFloatingOverlay';
 import ProductCardOne from '@/components/sections/product/ProductCardOne';
@@ -42,17 +42,31 @@ export default function LandingPage() {
   </div>
 
   <div id="hero" data-section="hero">
-      <HeroBillboard
+      <HeroSplitDoubleCarousel
       background={{ variant: "gradient-bars" }}
       title="Fikirlerinizi Giydiriyoruz: NEWMODA TEXTILE ile Anahtar Teslim Üretim"
       description="Kumaş seçiminden kalıp hazırlamaya, baskıdan paketlemeye kadar tüm süreçleri markanıza özel yönetiyoruz."
-      imageSrc="http://img.b2bpic.net/free-photo/woman-working-sewing-machine_23-2148898079.jpg"
-      imageAlt="Üretim Süreci"
+      leftCarouselItems={[
+        { imageSrc: "http://img.b2bpic.net/free-photo/woman-working-sewing-machine_23-2148898079.jpg", imageAlt: "Üretim Süreci 1" },
+        { imageSrc: "http://img.b2bpic.net/free-photo/working-with-team-about-new-dress_329181-14324.jpg", imageAlt: "Üretim Süreci 2" },
+        { imageSrc: "http://img.b2bpic.net/free-photo/portrait-businesswoman-owner-working_23-2148828334.jpg", imageAlt: "Üretim Süreci 3" },
+        { imageSrc: "http://img.b2bpic.net/free-photo/front-view-tailoring-studio-with-sewing-machine-garments_23-2148834125.jpg", imageAlt: "Üretim Süreci 4" },
+        { imageSrc: "http://img.b2bpic.net/free-photo/man-servant-ironing-clothing_23-2149530868.jpg", imageAlt: "Üretim Süreci 5" },
+      ]}
+      rightCarouselItems={[
+        { imageSrc: "http://img.b2bpic.net/free-photo/close-up-woman-holding-patterns_23-2148903486.jpg", imageAlt: "Showroom 1" },
+        { imageSrc: "http://img.b2bpic.net/free-photo/high-angle-different-threads-with-copy-space_23-2148876423.jpg", imageAlt: "Showroom 2" },
+        { imageSrc: "http://img.b2bpic.net/free-photo/man-cutter-sewing-factory-male-hands-close-up_1303-15849.jpg", imageAlt: "Showroom 3" },
+        { imageSrc: "http://img.b2bpic.net/free-photo/creative-atelier-shop-workspace_482257-95822.jpg", imageAlt: "Showroom 4" },
+        { imageSrc: "http://img.b2bpic.net/free-photo/side-view-woman-garage-sale_23-2150574511.jpg", imageAlt: "Showroom 5" },
+      ]}
       buttons={[{ text: "Üretim Hakkında Bilgi Alın", href: "https://wa.me/905330546773?text=Merhaba%20NEWMODA%20TEXTİLE%20ile%20üretim%20hakkında%20bilgi%20almak%20istiyorum." }]}
       avatars={[
-        { src: "http://img.b2bpic.net/free-photo/studio-portrait-bearded-hipster-glasses-wearing-casual_613910-12290.jpg", alt: "Ekip" },
-        { src: "http://img.b2bpic.net/free-photo/portrait-handsome-confident-businessman-black-wall_176420-3460.jpg", alt: "Ekip" },
-        { src: "http://img.b2bpic.net/free-photo/portrait-young-confident-man_176474-85919.jpg", alt: "Ekip" },
+        { src: "http://img.b2bpic.net/free-photo/studio-portrait-bearded-hipster-glasses-wearing-casual_613910-12290.jpg", alt: "Ekip Üyesi 1" },
+        { src: "http://img.b2bpic.net/free-photo/portrait-handsome-confident-businessman-black-wall_176420-3460.jpg", alt: "Ekip Üyesi 2" },
+        { src: "http://img.b2bpic.net/free-photo/portrait-young-confident-man_176474-85919.jpg", alt: "Ekip Üyesi 3" },
+        { src: "http://img.b2bpic.net/free-photo/successful-confident-businessman-glasses-speaking-beige-wall_176420-129.jpg", alt: "Ekip Üyesi 4" },
+        { src: "http://img.b2bpic.net/free-photo/smiling-middle-aged-business-leader-window_1262-5674.jpg", alt: "Ekip Üyesi 5" },
       ]}
       avatarText="100+ Başarılı Proje"
       marqueeItems={[
@@ -100,12 +114,12 @@ export default function LandingPage() {
       textboxLayout="default"
       useInvertedBackground={true}
       metrics={[
-        { id: "m1", value: "Modelhane", title: "Tasarımlarınızın başlangıç noktası.", description: "Detaylara önem veren uzman yaklaşım.", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Chc2qswBE8pMYFiBWnY5G8qpZC/uploaded-1776850577533-cm0mppv7.png" },
-        { id: "m2", value: "Kesim Atölyesi", title: "Hassas üretim disiplini.", description: "Mükemmel kesim teknolojileri.", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Chc2qswBE8pMYFiBWnY5G8qpZC/uploaded-1776851170062-zaln9hhy.png" },
-        { id: "m3", value: "Dikiş Hattı", title: "Profesyonel işçilik.", description: "Avrupa standartlarında üretim.", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Chc2qswBE8pMYFiBWnY5G8qpZC/uploaded-1776851128376-r57dc5rr.png" },
-        { id: "m4", value: "Ütü ve Paketleme", title: "Son kontroller ve etiketleme.", description: "Teslimata hazır kusursuz ürünler.", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Chc2qswBE8pMYFiBWnY5G8qpZC/uploaded-1776850305171-ij8gji8p.png" },
-        { id: "m5", value: "Showroom", title: "Ürünlerimizi inceleyin.", description: "Kurumsal ve profesyonel ortam.", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Chc2qswBE8pMYFiBWnY5G8qpZC/uploaded-1776851106747-mxdd3knl.jpg" },
-        { id: "m6", value: "Yönetim Ofisi", title: "Operasyonel güç merkezi.", description: "Kurumsal stratejiler.", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3Chc2qswBE8pMYFiBWnY5G8qpZC/uploaded-1776851794719-iqvi078i.png" },
+        { id: "m1", value: "Modelhane", title: "Tasarımlarınızın başlangıç noktası.", description: "Detaylara önem veren uzman yaklaşım.", imageSrc: "http://img.b2bpic.net/free-photo/medium-shot-woman-repairing-clothes_23-2150746451.jpg" },
+        { id: "m2", value: "Kesim Atölyesi", title: "Hassas üretim disiplini.", description: "Mükemmel kesim teknolojileri.", imageSrc: "http://img.b2bpic.net/free-photo/fashion-designer-working-her-workshop-alone_23-2148877343.jpg" },
+        { id: "m3", value: "Dikiş Hattı", title: "Profesyonel işçilik.", description: "Avrupa standartlarında üretim.", imageSrc: "http://img.b2bpic.net/free-photo/sewing-machine-accessories-workdesk-shop_23-2147874993.jpg" },
+        { id: "m4", value: "Ütü ve Paketleme", title: "Son kontroller ve etiketleme.", description: "Teslimata hazır kusursuz ürünler.", imageSrc: "http://img.b2bpic.net/free-photo/man-choosing-tie-shirt_1098-21878.jpg" },
+        { id: "m5", value: "Showroom", title: "Ürünlerimizi inceleyin.", description: "Kurumsal ve profesyonel ortam.", imageSrc: "http://img.b2bpic.net/free-photo/various-type-fabric-clothes-shop_23-2148175735.jpg" },
+        { id: "m6", value: "Yönetim Ofisi", title: "Operasyonel güç merkezi.", description: "Kurumsal stratejiler.", imageSrc: "http://img.b2bpic.net/free-photo/still-life-fashion-designer-s-office_23-2150543693.jpg" },
       ]}
       title="Fabrikamızdan Görüntüler"
       description="Üretim birimlerimiz ve yönetim ofisimizden detaylar."
